8eca5a482d201b9be71bb743148008fbd5e3e3e0
kkannan <kkannan@PC-BANG-615E>
2017-01-31 15:34:40 +0530

    Commiting UI Changes

--- src/app/CalLicenseDemo/CalLicenseDemo/ViewModel/BaseEntity.cs
+++ src/app/CalLicenseDemo/CalLicenseDemo/ViewModel/BaseEntity.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CalLicenseDemo.ViewModel
 {
@@ -14,6 +15,27 @@ namespace CalLicenseDemo.ViewModel
         public void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action<object> _action;
+
+        public RelayCommand(Action<object> action)
+        {
+            this._action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke(parameter);
         }
     }
 }
