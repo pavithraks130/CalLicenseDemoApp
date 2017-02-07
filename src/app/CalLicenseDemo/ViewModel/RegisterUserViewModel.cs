dea0fcdaf707d97ed2a699c565d698f775d41c27
CShettyFluke <cshetty@PC-BANG-6C>
2017-02-07 13:02:56 +0530

    Project documention and User input  validation

--- src/app/CalLicenseDemo/ViewModel/RegisterUserViewModel.cs
+++ src/app/CalLicenseDemo/ViewModel/RegisterUserViewModel.cs
@@ -5,10 +5,12 @@ using System.Windows.Navigation;
 using CalLicenseDemo.Logic;
 using CalLicenseDemo.Model;
 using CalLicenseDemo.ViewModel;
+using System.ComponentModel;
+using System;
 
 namespace CalLicenseDemo.Common
 {
-    internal class RegisterUserViewModel : BaseEntity
+    internal class RegisterUserViewModel : BaseEntity, IDataErrorInfo
     {
         private readonly RegistrationModel user;
         public string FName
@@ -61,48 +63,129 @@ namespace CalLicenseDemo.Common
             }
         }
 
-
         public ICommand RegisterCommand { get; set; }
 
         public NavigationService Service { get; set; }
 
+        public string Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
 
+        public string this[string columnName]
+        {
+            get
+            {
+                if ("Email" == columnName)
+                {
+                    return EmailValidation();
+                }
+                if ("Password" == columnName)
+                {
+                    return PasswordValidation();
+                }
+                if ("FName" == columnName)
+                {
+                    return FirstNameValidation();
+                }
+                if ("LName" == columnName)
+                {
+                    return LastNameValidation();
+                }
+                if ("Organization" == columnName)
+                {
+                    return OrganizationValidation();
+                }
+                return string.Empty;
+            }
+        }
+
+        private string LastNameValidation()
+        {
+            if (string.IsNullOrEmpty(LName))
+            {
+                return "Last name field is empty";
+            }
+            return string.Empty;
+        }
+
+        private string FirstNameValidation()
+        {
+            if (string.IsNullOrEmpty(FName))
+            {
+                return "First name field is empty";
+            }
+            return string.Empty;
+        }
+
+        private string OrganizationValidation()
+        {
+            if (string.IsNullOrEmpty(Organization))
+            {
+                return "Organization field is empty";
+            }
+            return string.Empty;
+        }
+
+        private string EmailValidation()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                return "Email address field is empty";
+            }
+            else if (!DataValidations.IsValidEmailId(Email))
+            {
+                return "Please enter valid email address";
+            }
+            return string.Empty;
+
+        }
+
+        private string PasswordValidation()
+        {
+            if (string.IsNullOrEmpty(Password))
+            {
+                return "Password field is empty";
+            }
+            return string.Empty;
+
+        }
         public RegisterUserViewModel(NavigationService service)
         {
             user = new RegistrationModel();
             Service = service;
             RegisterCommand = new RelayCommand(RegisterNewUser);
         }
-
         private void RegisterNewUser(object param)
         {
-            //if (Email == null || Email == "" || !DataValidations.IsValidEmailId(Email))
-            //{
-            //    MessageBox.Show("Please enter valid email address", "Warning");
-            //    return;
-            //}
-            var userLogic = new LoginLogic();
-            //Validate Email
-            var status = userLogic.ValidateEmail(user.Email);
-
-            if (status)
-            {
-                userLogic.CreateUserInfo(user);
-                string licenseType = param.ToString();
-                if (licenseType == "Trial")
+            if (!string.IsNullOrEmpty(LName) && !string.IsNullOrEmpty(FName) && !string.IsNullOrEmpty(Organization) && !string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Password))
                 {
-                    var logic = new LicenseLogic();
-                    SingletonLicense.Instance.SelectedSubscription = logic.GetTrialLicense();
-                    logic.ActivateSubscription();
+                var userLogic = new LoginLogic();
+                //Validate Email
+                var status = userLogic.ValidateEmail(user.Email);
+
+                if (status)
+                {
+                    userLogic.CreateUserInfo(user);
+                    string licenseType = param.ToString();
+                    if (licenseType == "Trial")
+                    {
+                        var logic = new LicenseLogic();
+                        SingletonLicense.Instance.SelectedSubscription = logic.GetTrialLicense();
+                        logic.ActivateSubscription();
+                    }
+                    var dict = new Dictionary<string, string>();
+                    dict.Add("licenseType", licenseType);
+                    if (NavigateNextPage != null)
+                        NavigateNextPage(null, dict);
                 }
-                var dict = new Dictionary<string, string>();
-                dict.Add("licenseType", licenseType);
-                if (NavigateNextPage != null)
-                    NavigateNextPage(null, dict);
-            }
-            else
-            {
-                MessageBox.Show("Email Address already Exist");
+                else
+                {
+                    MessageBox.Show("Email Address already Exist");
+                }
             }
         }
     }
