2808555e826080475a3bff97f4500cffaac5f1fb
kkannan <kkannan@PC-BANG-615E>
2017-02-07 08:34:42 +0530

    User Related details changed Feature List updated in dashboard

--- src/app/CalLicenseDemo/Common/StringToListConverter.cs
+++ src/app/CalLicenseDemo/Common/StringToListConverter.cs
@@ -14,10 +14,17 @@ namespace CalLicenseDemo.Common
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var featureList = (ICollection<Feature>)value;
-            StringBuilder str = new StringBuilder();
-            foreach (var feature in featureList)
-                str.Append(feature.FeatureTitle + ", ");
-            return str.ToString().Remove(str.Length - 2);
+            if (featureList != null && featureList.Count > 0)
+            {
+                StringBuilder str = new StringBuilder();
+                foreach (var feature in featureList)
+                    str.Append(feature.FeatureTitle + ", ");
+                return str.ToString().Remove(str.Length - 2);
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
